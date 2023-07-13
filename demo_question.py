646eb8b670fc52fb585dfa3e27906a83a76cc856
GadoyaDhruvilSanjayb <gadoyadhruvil0@gmail.com>
2023-07-13 11:36:52 +0530

    Added Demo question

---
+++ demo_question.py
@@ -0,0 +1,28 @@
+# demo_question.py
+
+"""
+Question:
+Given a list of numbers, write a Python function to return a new list that contains only the even numbers from the original list.
+
+Example:
+Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Output: [2, 4, 6, 8, 10]
+"""
+
+# Creating a Function
+def get_even_numbers(numbers):
+    # TODO: Implement the logic to filter even numbers from the list
+    pass
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = get_even_numbers(numbers)
+
+# Printing Output
+print(even_numbers)
+
+'''
+
+# Explanation:
+# The code presents a problem where a list of numbers is given, and the task is to create a new list containing only the even numbers from the original list. It provides an incomplete function named 'get_even_numbers' that needs to be implemented to solve the problem.
+
+'''
